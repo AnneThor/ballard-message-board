ecd3d015c06908feb7ed102c702155eebb42760b
Anne Thor <anne.thor@gmail.com>
2018-08-18 13:46:41 -0700

    assignment routing controllers view completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -3,18 +3,25 @@ const server = require("../../src/server");
 const base = "http://localhost:3000";
 
 describe("routes : static", () => {
-  //#1
+
   describe("GET /", () => {
-    //#2
     it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
-      //#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("Welcome to Bloccit");
-        //#4
         done();
       });
     });
   });
+
+  describe("GET /about", () => {
+    it("should return status code 200 and have 'About us' in the body of the response", (done) => {
+      request.get(`${base}/about`, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("About Us");
+        done();
+      })
+    })
+  })
 
 })
