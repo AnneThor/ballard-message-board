9ebe44e59c0b6aef1e70425c2c4d6f37f5589246
Anne Thor <anne.thor@gmail.com>
2018-08-18 11:28:45 -0700

    WIP deploy to production

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+const appConfig = require("./config/main-config.js");
+appConfig.init();
+
 const routeConfig = require("./config/route-config.js");
 routeConfig.init(app);
 
