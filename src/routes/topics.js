a2c8f7cd271a5215bdc540aacf4fcca2464305ae
Anne Thor <anne.thor@gmail.com>
2018-08-27 23:39:30 -0700

    assignment topic resource 2 completed

--- src/routes/topics.js
+++ src/routes/topics.js
@@ -5,11 +5,11 @@ const topicController = require("../controllers/topicController");
 
 router.get("/", topicController.index);
 router.get("/topics", topicController.index);
+router.post("/topics/create", topicController.create);
 router.get("/topics/new", topicController.new);
 router.get("/topics/:id", topicController.show);
-router.get("/topics/:id/edit", topicController.edit);
-router.post("/topics/create", topicController.create);
 router.post("/topics/:id/destroy", topicController.destroy);
+router.get("/topics/:id/edit", topicController.edit);
 router.post("/topics/:id/update", topicController.update);
 
 module.exports = router;
