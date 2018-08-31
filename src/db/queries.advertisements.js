42bc5e05e920af64e5ebb96af1dc8e55f71ea391
Anne Thor <anne.thor@gmail.com>
2018-08-30 21:39:22 -0700

    checkpoint-post-resource1 completed

---
+++ src/db/queries.advertisements.js
@@ -0,0 +1,68 @@
+const Advertisement = require("./models").Advertisement;
+
+module.exports = {
+
+  addAdvertisement(newAdvertisement, callback) {
+    return Advertisement.create({
+      title: newAdvertisement.title,
+      description: newAdvertisement.description
+    })
+    .then( (advertisement) => {
+      callback(null, advertisement);
+    })
+    .catch( (err) => {
+      callback(err);
+    })
+  },
+
+  deleteAdvertisement(id, callback) {
+    return Advertisement.destroy({
+      where: {id}
+    })
+    .then( (advertisement) => {
+      callback(null, advertisement);
+    })
+    .catch( (err) => {
+      callback(err);
+    })
+  },
+
+  getAdvertisement(id, callback) {
+    return Advertisement.findById(id)
+    .then( (advertisement) => {
+      callback(null, advertisement);
+    })
+    .catch( (err) => {
+      callback(err);
+    })
+  },
+
+  getAllAdvertisements(callback) {
+    return Advertisement.all()
+    .then( (advertisements) => {
+      callback(null, advertisements);
+    })
+    .catch( (err) => {
+      callback(err);
+    })
+  },
+
+  updateAdvertisement(id, updatedAdvertisement, callback) {
+    return Advertisement.findById(id)
+    .then( (advertisement) => {
+      if(!advertisement) {
+        return callback("Topic not found");
+      }
+      advertisement.update(updatedAdvertisement, {
+        fields: Object.keys(updatedAdvertisement)
+      })
+      .then( () => {
+        callback(null, advertisement);
+      })
+      .catch( (err) => {
+        callback(err);
+      });
+    });
+  },
+
+}
