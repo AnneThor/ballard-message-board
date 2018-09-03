19f9e2c64f5ff8251048b79f0059b9a86a2a6217
Anne Thor <anne.thor@gmail.com>
2018-09-02 23:41:28 -0700

    checkpoint post resource 2 completed

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,11 +1,12 @@
 module.exports = {
 
   init(app) {
+      const postRoutes = require("../routes/posts");
       const staticRoutes = require("../routes/static");
       const topicRoutes = require("../routes/topics");
+      app.use(postRoutes);
       app.use(staticRoutes);
       app.use(topicRoutes);
   }
-
 
 }
