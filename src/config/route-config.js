d55c344a6dd3c57c63881037f53b7fbf79389d05
Anne Thor <anne.thor@gmail.com>
2018-09-05 21:46:48 -0700

    checkpoint authentication completed

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -4,9 +4,11 @@ module.exports = {
       const postRoutes = require("../routes/posts");
       const staticRoutes = require("../routes/static");
       const topicRoutes = require("../routes/topics");
+      const userRoutes = require("../routes/users");
       app.use(postRoutes);
       app.use(staticRoutes);
       app.use(topicRoutes);
+      app.use(userRoutes);
   }
 
 }
